16aedaccc619bd0923ec5b39022ccf14daf919f1
AleksandrovVladislav <473335qwerty@gmail.com>
2025-04-01 14:09:13 +0300

    Add files via upload

---
+++ Тесты Чёрного наречия.py
@@ -0,0 +1,55 @@
+from Чёрное_наречие import *
+code = '''
+ert i = 0
+margim(i)
+'''
+Black_Speech(code)
+
+code2 = '''
+hu-na(i)
+margim(i)
+'''
+Black_Speech(code2)
+
+code3 = '''
+i = ['0', 0, 0]
+margim(int(i[1]), str(i[2]), float(i[3]))
+'''
+Black_Speech(code3)
+
+code4 = '''
+s = 'qwertyuiop'
+margim(sigum(s), s.bhad(q, ''), s.sorgh(), s.gukh(), s.talug())
+'''
+Black_Speech(code4)
+
+code5 = '''
+s = []
+margim(s.marbh(abc), )
+'''
+Black_Speech(code5)
+
+code6 = '''
+ar i dau dunudum(10):
+    margim(i)
+    atishum
+    
+'''
+Black_Speech(code6)
+
+code7 = '''
+ert i = 0
+ert r = kogurz
+fal kogurz:
+    i += 1
+    r = fururz
+    
+'''
+Black_Speech(code7)
+
+code8 = '''
+horm fasharz
+margim(fasharz.asharz(1,10))
+'''
+Black_Speech(code8)
+
